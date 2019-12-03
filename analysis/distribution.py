b7605129dd1d6aa6cad50213c408b4a7ec560781
Garret Rieger <grieger@google.com>
2019-12-02 16:01:12 -0800

    When encoding distribution buckets with gaps, insert the previous bucket so we can tell where the bucket starts.

--- analysis/distribution.py
+++ analysis/distribution.py
@@ -20,12 +20,22 @@ class Distribution:
 
   def to_proto(self):
     """Convert this distribution to a DistributionProto."""
+    previous_end = 0
     distribution = result_pb2.DistributionProto()
     for end, count in self.buckets.items():
+      if end != self.bucketer.bucket_for(previous_end):
+        # When there's a gap the previous bucket should be included
+        # so the start of the bucket can be determined.
+        bucket = result_pb2.BucketProto()
+        bucket.end = self.bucketer.bucket_before(end - 1)
+        bucket.count = 0
+        distribution.buckets.append(bucket)
+
       bucket = result_pb2.BucketProto()
       bucket.end = end
       bucket.count = count
       distribution.buckets.append(bucket)
+      previous_end = end
     return distribution
 
 
@@ -37,3 +47,8 @@ class LinearBucketer:
 
   def bucket_for(self, value):
     return (int(value / self.width) + 1) * self.width
+
+  def bucket_before(self, value):
+    if value >= self.width:
+      return self.bucket_for(value - self.width)
+    return 0
--- analysis/distribution_test.py
+++ analysis/distribution_test.py
@@ -9,12 +9,36 @@ from analysis import result_pb2
 class DistributionTest(unittest.TestCase):
 
   def test_linear_bucketer(self):
+    bucketer = distribution.LinearBucketer(1)
+    self.assertEqual(bucketer.bucket_for(0), 1)
+    self.assertEqual(bucketer.bucket_for(5), 6)
+
     bucketer = distribution.LinearBucketer(10)
     self.assertEqual(bucketer.bucket_for(0), 10)
     self.assertEqual(bucketer.bucket_for(1), 10)
     self.assertEqual(bucketer.bucket_for(9), 10)
     self.assertEqual(bucketer.bucket_for(10), 20)
     self.assertEqual(bucketer.bucket_for(101), 110)
+
+  def test_linear_bucketer_bucket_before(self):
+    bucketer = distribution.LinearBucketer(1)
+    self.assertEqual(bucketer.bucket_before(0), 0)
+    self.assertEqual(bucketer.bucket_before(5), 5)
+
+    bucketer = distribution.LinearBucketer(10)
+    self.assertEqual(bucketer.bucket_before(0), 0)
+    self.assertEqual(bucketer.bucket_before(1), 0)
+    self.assertEqual(bucketer.bucket_before(9), 0)
+
+    self.assertEqual(bucketer.bucket_before(10), 10)
+    self.assertEqual(bucketer.bucket_before(11), 10)
+    self.assertEqual(bucketer.bucket_before(19), 10)
+
+    self.assertEqual(bucketer.bucket_before(20), 20)
+    self.assertEqual(bucketer.bucket_before(21), 20)
+    self.assertEqual(bucketer.bucket_before(29), 20)
+
+    self.assertEqual(bucketer.bucket_before(101), 100)
 
   def test_add_value(self):
     dist = distribution.Distribution(distribution.LinearBucketer(10))
@@ -26,6 +50,33 @@ class DistributionTest(unittest.TestCase):
     dist.add_value(101)
 
     self.assertEqual(dist.buckets, {10: 3, 20: 1, 110: 1})
+
+  def test_to_proto_one_wide(self):
+    dist = distribution.Distribution(distribution.LinearBucketer(1))
+    dist.add_value(0)
+    dist.add_value(9)
+    dist.add_value(10)
+
+    dist_proto = result_pb2.DistributionProto()
+
+    bucket = result_pb2.BucketProto()
+    bucket.end = 1
+    bucket.count = 1
+    dist_proto.buckets.append(bucket)
+    bucket = result_pb2.BucketProto()
+    bucket.end = 9
+    bucket.count = 0
+    dist_proto.buckets.append(bucket)
+    bucket = result_pb2.BucketProto()
+    bucket.end = 10
+    bucket.count = 1
+    dist_proto.buckets.append(bucket)
+    bucket = result_pb2.BucketProto()
+    bucket.end = 11
+    bucket.count = 1
+    dist_proto.buckets.append(bucket)
+
+    self.assertEqual(dist.to_proto(), dist_proto)
 
   def test_to_proto(self):
     dist = distribution.Distribution(distribution.LinearBucketer(10))
@@ -45,8 +96,30 @@ class DistributionTest(unittest.TestCase):
     bucket.end = 20
     bucket.count = 1
     dist_proto.buckets.append(bucket)
+
+    bucket = result_pb2.BucketProto()
+    bucket.end = 100
+    bucket.count = 0
+    dist_proto.buckets.append(bucket)
     bucket = result_pb2.BucketProto()
     bucket.end = 110
+    bucket.count = 1
+    dist_proto.buckets.append(bucket)
+
+    self.assertEqual(dist.to_proto(), dist_proto)
+
+  def test_to_proto_with_starting_gap(self):
+    dist = distribution.Distribution(distribution.LinearBucketer(10))
+
+    dist.add_value(21)
+
+    dist_proto = result_pb2.DistributionProto()
+    bucket = result_pb2.BucketProto()
+    bucket.end = 20
+    bucket.count = 0
+    dist_proto.buckets.append(bucket)
+    bucket = result_pb2.BucketProto()
+    bucket.end = 30
     bucket.count = 1
     dist_proto.buckets.append(bucket)
 
