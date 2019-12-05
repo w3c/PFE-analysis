c339670f151516a8830cf30523e1035ae9e46ea4
Garret Rieger <grieger@google.com>
2019-12-05 10:23:35 -0800

    Finish implementation of strategy loader.

--- analysis/pfe_methods/unicode_range_data/slicing_strategy_loader.py
+++ analysis/pfe_methods/unicode_range_data/slicing_strategy_loader.py
@@ -1,16 +1,36 @@
 """Helper that can load slicing strategy protos by name."""
 
+import io
 import os
 
 from analysis.pfe_methods.unicode_range_data import slicing_strategy_pb2
+from fontTools import ttLib
 from google.protobuf import text_format
 
 SLICING_STRATEGY_DIR = "analysis/pfe_methods/unicode_range_data"
 
+CACHE = dict()
+
 
 def slicing_strategy_for_font(font_bytes):  # pylint: disable=unused-argument
   """Determines which slicing strategy should be used for the given font."""
-  # TODO(garretrieger): Implement me!
+
+  # Slicing strategy is picked by counting what % of the codepoints in the font
+  # are covered by each available strategy. Choose the strategy with the highest
+  # coverage.
+  codepoints = codepoints_in_font(font_bytes)
+  strategy_scores = dict()
+  for strategy_name in get_available_strategies():
+    strategy = load_slicing_strategy(strategy_name)
+    all_strategy_codepoints = set()
+    for subset in strategy:
+      all_strategy_codepoints.update(subset)
+
+    strategy_scores[strategy_name] = sum(
+        codepoint in all_strategy_codepoints for codepoint in codepoints)
+
+  return max(strategy_scores.keys(),
+             key=lambda strategy: strategy_scores[strategy])
 
 
 def get_available_strategies():
@@ -24,6 +44,9 @@ def get_available_strategies():
 
 def load_slicing_strategy(strategy_name):
   """Load the slicing strategy identified by strategy_name and return it."""
+  if strategy_name in CACHE:
+    return CACHE[strategy_name]
+
   file_name = "%s.textproto" % strategy_name
   with open(os.path.join(SLICING_STRATEGY_DIR, file_name),
             'r') as strategy_file:
@@ -32,7 +55,10 @@ def load_slicing_strategy(strategy_name):
   strategy_proto = slicing_strategy_pb2.SlicingStrategy()
   text_format.Merge(strategy_file_contents, strategy_proto)
 
-  return [subset_to_set(subset) for subset in strategy_proto.subsets]
+  result = [subset_to_set(subset) for subset in strategy_proto.subsets]
+
+  CACHE[strategy_name] = result
+  return result
 
 
 def subset_to_set(subset_proto):
@@ -41,4 +67,11 @@ def subset_to_set(subset_proto):
 
 def codepoints_in_font(font_bytes):  # pylint: disable=unused-argument
   """Returns the set of codepoints that the font can render."""
-  # TODO(garretrieger): Implement me!
+  font = ttLib.TTFont(io.BytesIO(font_bytes))
+
+  result = set()
+  for sub_table in font["cmap"].tables:
+    if sub_table.isUnicode():
+      result.update(sub_table.cmap.keys())
+
+  return result
--- analysis/pfe_methods/unicode_range_data/slicing_strategy_loader_test.py
+++ analysis/pfe_methods/unicode_range_data/slicing_strategy_loader_test.py
@@ -7,6 +7,14 @@ from analysis.pfe_methods.unicode_range_data import slicing_strategy_loader
 
 class SlicingStrategyLoaderTest(unittest.TestCase):
 
+  def test_load_slicing_strategy_caches(self):
+    strategy_1 = slicing_strategy_loader.load_slicing_strategy("non_cjk_slices")
+    strategy_2 = slicing_strategy_loader.load_slicing_strategy("non_cjk_slices")
+    strategy_3 = slicing_strategy_loader.load_slicing_strategy(
+        "japanese_slices")
+    self.assertIs(strategy_1, strategy_2)
+    self.assertNotEqual(strategy_1, strategy_3)
+
   def test_load_slicing_strategy(self):
     # A slicing strategy should be a list of sets of integers
     strategy = slicing_strategy_loader.load_slicing_strategy("non_cjk_slices")
@@ -29,6 +37,29 @@ class SlicingStrategyLoaderTest(unittest.TestCase):
   def test_all_available_strategies_can_load(self):
     for strategy in slicing_strategy_loader.get_available_strategies():
       self.assertTrue(slicing_strategy_loader.load_slicing_strategy(strategy))
+
+  def test_codepoints_in_font(self):
+    with open("patch_subset/testdata/Roboto-Regular.abcd.ttf",
+              "rb") as font_file:
+      font_bytes = font_file.read()
+
+    codepoints = slicing_strategy_loader.codepoints_in_font(font_bytes)
+    self.assertEqual(codepoints, {0x61, 0x62, 0x63, 0x64})
+
+  def test_slicing_strategy_for_font_non_cjk(self):
+    with open("patch_subset/testdata/Roboto-Regular.ttf", "rb") as font_file:
+      font_bytes = font_file.read()
+    self.assertEqual(
+        "non_cjk_slices",
+        slicing_strategy_loader.slicing_strategy_for_font(font_bytes))
+
+  def test_slicing_strategy_for_font_cjk(self):
+    with open("patch_subset/testdata/NotoSansJP-Regular.otf",
+              "rb") as font_file:
+      font_bytes = font_file.read()
+    self.assertEqual(
+        "japanese_slices",
+        slicing_strategy_loader.slicing_strategy_for_font(font_bytes))
 
 
 if __name__ == '__main__':
