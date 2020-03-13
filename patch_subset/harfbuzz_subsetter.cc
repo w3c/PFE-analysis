602974a2cca49cf49892995b028cc2759ca4788c
Garret Rieger <grieger@google.com>
2020-03-13 15:37:36 -0700

    Don't use retain gids for fonts with large numbers of codepoints.

--- patch_subset/harfbuzz_subsetter.cc
+++ patch_subset/harfbuzz_subsetter.cc
@@ -3,9 +3,18 @@
 #include "common/logging.h"
 #include "common/status.h"
 #include "hb-subset.h"
+#include "hb_set_unique_ptr.h"
 #include "patch_subset/font_data.h"
 
 namespace patch_subset {
+
+static unsigned int kRetainGidsThreshold = 5000;
+
+bool HarfbuzzSubsetter::ShouldRetainGids(const FontData& font) const {
+  hb_set_unique_ptr codepoints = make_hb_set();
+  CodepointsInFont(font, codepoints.get());
+  return hb_set_get_population(codepoints.get()) < kRetainGidsThreshold;
+}
 
 StatusCode HarfbuzzSubsetter::Subset(const FontData& font,
                                      const hb_set_t& codepoints,
@@ -23,9 +32,12 @@ StatusCode HarfbuzzSubsetter::Subset(const FontData& font,
   hb_set_t* input_codepoints = hb_subset_input_unicode_set(input);
   hb_set_union(input_codepoints, &codepoints);
 
-  // For subsetting we want to retain glyph ids, this helps reduce
-  // patch size by keeping the ids consistent between patches.
-  hb_subset_input_set_retain_gids(input, true);
+  // For subsetting we want normally to retain glyph ids, since this helps
+  // reduce patch size by keeping the ids consistent between patches. However
+  // for fonts with large numbers of codepoints the additional overhead added
+  // encoding the empty gids is not worth the future savings on patch size.
+  hb_subset_input_set_retain_gids(input, ShouldRetainGids(font));
+
   // hb-subset by default drops layout tables, keep them.
   hb_set_del(hb_subset_input_drop_tables_set(input),
              HB_TAG('G', 'S', 'U', 'B'));
