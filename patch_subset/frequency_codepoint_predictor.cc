e7f98662a55bffcecc604a8fb8ec677f06bae1f5
Garret Rieger <grieger@google.com>
2020-06-22 16:15:01 -0700

    More implementation of FrequencyCodepointPredictor.

--- patch_subset/frequency_codepoint_predictor.cc
+++ patch_subset/frequency_codepoint_predictor.cc
@@ -7,17 +7,27 @@
 #include <iterator>
 #include <vector>
 
+#include "absl/container/btree_set.h"
 #include "analysis/pfe_methods/unicode_range_data/slicing_strategy.pb.h"
 #include "common/logging.h"
 #include "common/status.h"
 
+using absl::btree_set;
+using analysis::pfe_methods::unicode_range_data::Codepoint;
 using analysis::pfe_methods::unicode_range_data::SlicingStrategy;
+using analysis::pfe_methods::unicode_range_data::Subset;
 using google::protobuf::TextFormat;
 
 namespace patch_subset {
 
 static const char* kSlicingStrategyDataDirectory =
     "analysis/pfe_methods/unicode_range_data/";
+
+struct CodepointFreqCompare {
+  bool operator()(const Codepoint& lhs, const Codepoint& rhs) const {
+    return lhs.count() > rhs.count();
+  }
+};
 
 StatusCode LoadStrategy(const std::string& path, SlicingStrategy* out) {
   std::ifstream input(path);
@@ -60,16 +70,55 @@ StatusCode LoadAllStrategies(std::vector<SlicingStrategy>* strategies) {
   return StatusCode::kOk;
 }
 
-FrequencyCodepointPredictor::FrequencyCodepointPredictor() {
-  LoadAllStrategies(&strategies);
+FrequencyCodepointPredictor* FrequencyCodepointPredictor::Create() {
+  std::vector<SlicingStrategy> strategies;
+  StatusCode result = LoadAllStrategies(&strategies);
+  if (result != StatusCode::kOk) {
+    return nullptr;
+  }
+
+  return new FrequencyCodepointPredictor(std::move(strategies));
 }
+
+FrequencyCodepointPredictor::FrequencyCodepointPredictor(
+    std::vector<SlicingStrategy> strategies)
+    : strategies_(std::move(strategies)) {}
 
 void FrequencyCodepointPredictor::Predict(
     const hb_set_t* font_codepoints, const hb_set_t* requested_codepoints,
     unsigned max, hb_set_t* predicted_codepoints /* OUT */) const {
-  // 1. Pick a strategy
-  // 2. Identify subsets that intersect codepoints.
-  // 3. Select the highest use codepoints that are not already present.
+  const SlicingStrategy& best_strategy = BestStrategyFor(font_codepoints);
+
+  btree_set<Codepoint, CodepointFreqCompare> additional_codepoints;
+  for (auto subset : best_strategy.subsets()) {
+    if (!Intersects(subset, requested_codepoints)) {
+      continue;
+    }
+
+    for (auto codepoint : subset.codepoint_frequencies()) {
+      additional_codepoints.insert(codepoint);
+
+      if (additional_codepoints.size() > max) {
+        // We only keep at most 'max' codepoints so remove the lowest frequency
+        // one.
+        additional_codepoints.erase(additional_codepoints.end());
+      }
+    }
+  }
+
+  for (auto codepoint : additional_codepoints) {
+    hb_set_add(predicted_codepoints, codepoint.codepoint());
+  }
+}
+
+bool FrequencyCodepointPredictor::Intersects(
+    const Subset& subset, const hb_set_t* requested_codepoints) const {
+  // TODO(garretrieger): Implement me!
+  return true;
+}
+
+const SlicingStrategy& FrequencyCodepointPredictor::BestStrategyFor(
+    const hb_set_t* codepoints) const {
   // TODO(garretrieger): Implement me!
 }
 
--- patch_subset/frequency_codepoint_predictor.h
+++ patch_subset/frequency_codepoint_predictor.h
@@ -16,15 +16,26 @@ namespace patch_subset {
  */
 class FrequencyCodepointPredictor : public CodepointPredictor {
  public:
-  FrequencyCodepointPredictor();
+  static FrequencyCodepointPredictor* Create();
 
   void Predict(const hb_set_t* font_codepoints,
                const hb_set_t* requested_codepoints, unsigned max,
                hb_set_t* predicted_codepoints /* OUT */) const;
 
  private:
+  FrequencyCodepointPredictor(
+      std::vector<analysis::pfe_methods::unicode_range_data::SlicingStrategy>
+          strategies);
+
+  bool Intersects(
+      const analysis::pfe_methods::unicode_range_data::Subset& subset,
+      const hb_set_t* requested_codepoints) const;
+
+  const analysis::pfe_methods::unicode_range_data::SlicingStrategy&
+  BestStrategyFor(const hb_set_t* codepoints) const;
+
   std::vector<analysis::pfe_methods::unicode_range_data::SlicingStrategy>
-      strategies;
+      strategies_;
 };
 
 }  // namespace patch_subset
