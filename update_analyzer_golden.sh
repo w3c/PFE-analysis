06f48fb84d3a557f0c21636fb2d2fa3579cbdb81
Garret Rieger <grieger@google.com>
2020-09-14 15:44:50 -0700

    Don't run range request by default. It requires a different version the data set and fonts (optimized by https://github.com/litherum/StreamableFonts) so it shouldn't be run with the other methods. Add a flag simulate_range_request which disables the other methods and only runs range request.

--- update_analyzer_golden.sh
+++ update_analyzer_golden.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-bazel run analysis:analyzer_integration_test -- --update_golden=$(pwd)/analysis/sample/english_sequence.result.textproto
+bazel run analysis:analyzer_integration_test -- \
+  --update_golden=$(pwd)/analysis/sample/english_sequence.result.textproto
+
+bazel run analysis:analyzer_integration_test -- \
+  --update_golden=$(pwd)/analysis/sample/range_request.english_sequence.result.textproto \
+  --simulate_range_request
