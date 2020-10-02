7436cbbe0abfbf2252a233c8f36a218e97a981a2
Andrew John <andyjohn@gmail.com>
2020-10-01 18:02:16 -0700

    Fix variable names.

--- analysis/font_loader_test.py
+++ analysis/font_loader_test.py
@@ -9,37 +9,37 @@ class FontLoaderTest(unittest.TestCase):
   '''Tests for font_loader.py'''
 
   def test_read_font(self):
-    with tempfile.TemporaryDirectory() as dir:
+    with tempfile.TemporaryDirectory() as the_dir:
       font_id = 'test.ttf'
       font_contents = 'font-bytes-go-here'
-      with open(dir + '/' + font_id, 'w') as tmp_file:
+      with open(the_dir + '/' + font_id, 'w') as tmp_file:
         tmp_file.write(font_contents)
 
-      bytes = font_loader.FontLoader(dir).load_font(font_id)
+      the_bytes = font_loader.FontLoader(the_dir).load_font(font_id)
 
-      self.assertEqual(str(bytes, encoding='UTF-8'), font_contents)
+      self.assertEqual(str(the_bytes, encoding='UTF-8'), font_contents)
 
   def test_read_variable_font(self):
-    with tempfile.TemporaryDirectory() as dir:
+    with tempfile.TemporaryDirectory() as the_dir:
       font_id = 'test[axis].ttf'
       font_contents = 'font-bytes-go-here'
-      with open(dir + '/' + font_id, 'w') as tmp_file:
+      with open(the_dir + '/' + font_id, 'w') as tmp_file:
         tmp_file.write(font_contents)
 
-      bytes = font_loader.FontLoader(dir).load_font(font_id)
+      the_bytes = font_loader.FontLoader(the_dir).load_font(font_id)
 
-      self.assertEqual(str(bytes, encoding='UTF-8'), font_contents)
+      self.assertEqual(str(the_bytes, encoding='UTF-8'), font_contents)
 
   def test_read_variable_renamed_font(self):
-    with tempfile.TemporaryDirectory() as dir:
+    with tempfile.TemporaryDirectory() as the_dir:
       font_id = 'test[axis].ttf'
       font_contents = 'font-bytes-go-here'
-      with open(dir + '/testBaxisB.ttf', 'w') as tmp_file:
+      with open(the_dir + '/testBaxisB.ttf', 'w') as tmp_file:
         tmp_file.write(font_contents)
 
-      bytes = font_loader.FontLoader(dir).load_font(font_id)
+      the_bytes = font_loader.FontLoader(the_dir).load_font(font_id)
 
-      self.assertEqual(str(bytes, encoding='UTF-8'), font_contents)
+      self.assertEqual(str(the_bytes, encoding='UTF-8'), font_contents)
 
 
 if __name__ == '__main__':
