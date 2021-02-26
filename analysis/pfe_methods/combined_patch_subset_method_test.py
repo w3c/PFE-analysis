4f914caa2b204da80418cd599f3bbd052aca80c5
Andy John <andyj@google.com>
2021-02-25 18:42:10 -0800

    Add lookup table of optimized settings.

--- analysis/pfe_methods/combined_patch_subset_method_test.py
+++ analysis/pfe_methods/combined_patch_subset_method_test.py
@@ -2,6 +2,8 @@
 from collections import namedtuple
 import unittest
 
+from absl import flags
+from absl.testing import flagsaver
 from analysis import font_loader
 from analysis import network_models
 from analysis.pfe_methods import combined_patch_subset_method
@@ -16,12 +18,13 @@ def u(codepoints):  # pylint: disable=invalid-name
 class CombinedPatchSubsetMethodTest(unittest.TestCase):
 
   def setUp(self):
+    flags.FLAGS(["auto-settings", "False"])
     self.latin_method = combined_patch_subset_method.CombinedPatchSubsetMethod(
         "latin")
     self.arabic_indic_method = combined_patch_subset_method.CombinedPatchSubsetMethod(
         "arabic_indic")
     self.cjk_method = combined_patch_subset_method.CombinedPatchSubsetMethod(
-        "cjk_indic")
+        "cjk")
     self.unknown_method = combined_patch_subset_method.CombinedPatchSubsetMethod(
         "invalid")
     self.font_loader = font_loader.FontLoader("./patch_subset/testdata/")
@@ -88,6 +91,22 @@ class CombinedPatchSubsetMethodTest(unittest.TestCase):
         desktop_session.get_request_graphs()[0].total_response_bytes(),
         twog_session.get_request_graphs()[0].total_response_bytes())
 
+  def test_differs_by_auto_settings_flag(self):
+    # Normal (non auto settings)
+    desktop_session_normal = self.latin_method.start_session(
+        network_models.DESKTOP_MEDIAN, self.font_loader)
+    usage = {"Roboto-Regular.ttf": u([0x61])}
+    desktop_session_normal.page_view(usage)
+
+    # Again with auto settings turned on.
+    with flagsaver.flagsaver(auto_settings=True):
+      desktop_session_auto = self.latin_method.start_session(
+          network_models.DESKTOP_MEDIAN, self.font_loader)
+      desktop_session_auto.page_view(usage)
+
+    self.assertNotEqual(
+        desktop_session_normal.get_request_graphs()[0].total_response_bytes(),
+        desktop_session_auto.get_request_graphs()[0].total_response_bytes())
 
 if __name__ == '__main__':
   unittest.main()
