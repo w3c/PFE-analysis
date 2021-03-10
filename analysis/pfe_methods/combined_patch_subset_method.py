1a6831248d9b96f5115b0a47635ee8da095e6bc5
Andy John <andyj@google.com>
2021-03-09 16:25:33 -0800

    Avoid too many branches

--- analysis/pfe_methods/combined_patch_subset_method.py
+++ analysis/pfe_methods/combined_patch_subset_method.py
@@ -65,6 +65,15 @@ def pick_method(network_model, script_category):  # pylint: disable=too-many-ret
       # Should not happen. Fall through to non-auto behavior.
       sys.stderr.write("MISSING KEY FOR " + network_model.name + " " +
                        script_category)
+  return pick_method_semiopt(network_model, script_category)
+
+
+def pick_method_semiopt(network_model, script_category):  # pylint: disable=too-many-return-statements
+  """Select best method based on the clients rtt and the script.
+
+  Picks the best set of codepoint prediction parameters for patch subset
+  based on the script being simulated and the clients round trip time.
+  """
   rtt = network_model.rtt
   if script_category == "latin":
     if 350 <= rtt <= 650:
