e98d1bcf5a61b7d28c32f0723dbc0ab3731ddbf8
Andrew John <45769922+andyjgf@users.noreply.github.com>
2021-05-19 11:07:33 -0700

    Formatting.

--- analysis/pfe_methods/combined_patch_subset_method_test.py
+++ analysis/pfe_methods/combined_patch_subset_method_test.py
@@ -56,12 +56,12 @@ class CombinedPatchSubsetMethodTest(unittest.TestCase):
 
   def test_differs_by_script(self):
     with flagsaver.flagsaver(auto_settings=True):
-        latin_session = self.latin_method.start_session(
-            network_models.MOBILE_2G_SLOWEST, self.font_loader)
-        cjk_session = self.cjk_method.start_session(
-            network_models.MOBILE_2G_SLOWEST, self.font_loader)
-        arabic_indic_session = self.arabic_indic_method.start_session(
-            network_models.MOBILE_2G_SLOWEST, self.font_loader)
+      latin_session = self.latin_method.start_session(
+          network_models.MOBILE_2G_SLOWEST, self.font_loader)
+      cjk_session = self.cjk_method.start_session(
+          network_models.MOBILE_2G_SLOWEST, self.font_loader)
+      arabic_indic_session = self.arabic_indic_method.start_session(
+          network_models.MOBILE_2G_SLOWEST, self.font_loader)
 
     usage = {"NotoSansJP-Regular.otf": u([0x61])}
     latin_session.page_view(usage)
@@ -72,7 +72,6 @@ class CombinedPatchSubsetMethodTest(unittest.TestCase):
         latin_session.get_request_graphs()[0].total_response_bytes(),
         arabic_indic_session.get_request_graphs()[0].total_response_bytes())
 
-
   def test_differs_by_network(self):
     desktop_session = self.latin_method.start_session(
         network_models.DESKTOP_MEDIAN, self.font_loader)
@@ -82,7 +81,6 @@ class CombinedPatchSubsetMethodTest(unittest.TestCase):
     usage = {"Roboto-Regular.ttf": u([0x61])}
     desktop_session.page_view(usage)
     twog_session.page_view(usage)
-
 
   def test_differs_by_auto_settings_flag(self):
     # Normal (non auto settings)
